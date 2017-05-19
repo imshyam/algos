0335e5397d2c085e1d92ee8eac55930eca6a41a3
Shyam <choudhary_s@worksap.co.jp>
2017-05-19 19:49:03 +0800

    show result

--- dp/RodCutting.java
+++ dp/RodCutting.java
@@ -2,26 +2,35 @@ package algos.dp;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 
 public class RodCutting {
 	public int price;
-	public int result[];
+	public ArrayList<Integer> result = new ArrayList<Integer>();
 
 	public void solve(int n, int[] p){
 		int tempSol[] = new int[n + 1];
+		int cutHere[] = new int[n + 1];
 		tempSol[0] = 0;
 		for(int i = 1; i < n + 1; i ++) {
 			int t = -1;
+			int t1 = i;
 			for(int j = 1; j < i + 1; j++){
-				t = max(t, p[j - 1] + tempSol[i - j]);
 				if(t < (p[j - 1] + tempSol[i - j])){
-					System.out.println("break at : " + j);
+					t1 = i - j;
 				}
+				t = max(t, p[j - 1] + tempSol[i - j]);
 			}
 			tempSol[i] = t;
+			cutHere[i] = t1;
 		}
 		this.price = tempSol[n];
+		do {
+			int res = r.cutHere[n] == 0 ? n - r.cutHere[n] : r.cutHere[n];
+			result.add(res);
+			n -= res;
+		} while(n > 0);
 	}
 
 	int max(int a, int b) {
--- test/Test.java
+++ test/Test.java
@@ -6,9 +6,9 @@ class Test{
 	public static void main(String args[]){
 		RodCutting r = new RodCutting();
 		int n; int[] p;
-		n = 10;
+		n = 9;
 		p = new int[]{1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
 		r.solve(n, p);
-		System.out.println(r.price);
+		System.out.println(r.result)
 	}
 }
