c160c4ac79cf6f62e53bac942701123052b3531a
Shyam <choudhary_s@worksap.co.jp>
2017-05-22 16:59:44 +0800

    add doc

--- dp/RodCutting.java
+++ dp/RodCutting.java
@@ -4,17 +4,35 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 
+/**
+  *
+  * @author imshyam
+  *
+  */
+
+/** Class contains :
+  * 1. price
+  *       long as maximum revenue obtainable by cutting up the rod and selling the pieces
+  * 2. result
+  *       ArrayList of length of the pieces
+  */
 
 public class RodCutting {
-	public int price;
+	// Maximum revenue obtainable by cutting up the rod and selling the pieces
+	public long price;
+	// Length of the pieces
 	public ArrayList<Integer> result = new ArrayList<Integer>();
 
 	public void solve(int n, int[] p){
+		// Array for memoizing prices of smaller cuts
 		int tempSol[] = new int[n + 1];
+		// Array for memoizing cut location
 		int cutHere[] = new int[n + 1];
 		tempSol[0] = 0;
 		for(int i = 1; i < n + 1; i ++) {
+			// Temp price of the piece of rod
 			int t = -1;
+			// Temp location of the cut
 			int t1 = i;
 			for(int j = 1; j < i + 1; j++){
 				if(t < (p[j - 1] + tempSol[i - j])){
@@ -27,8 +45,8 @@ public class RodCutting {
 		}
 		this.price = tempSol[n];
 		do {
-			int res = r.cutHere[n] == 0 ? n - r.cutHere[n] : r.cutHere[n];
-			result.add(res);
+			int res = cutHere[n] == 0 ? n - cutHere[n] : cutHere[n];
+			this.result.add(res);
 			n -= res;
 		} while(n > 0);
 	}
--- test/Test.java
+++ test/Test.java
@@ -6,9 +6,10 @@ class Test{
 	public static void main(String args[]){
 		RodCutting r = new RodCutting();
 		int n; int[] p;
-		n = 9;
+		n = 8;
 		p = new int[]{1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
 		r.solve(n, p);
-		System.out.println(r.result)
+		System.out.println(r.result);
+		System.out.println(r.price);
 	}
 }
