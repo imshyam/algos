89885e1050bf2656a18d58f610b39f331201b1f5
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-11-20 18:54:17 +0800

    complete inseration sort

--- CLRS/inseration_sort.cpp
+++ CLRS/inseration_sort.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void inseration_sort(int* a, int n) {
+	for (int i = 1; i < n; ++i)
+	{
+		int tmp = a[i];
+		for (int j = i; j >= 0; j--)
+		{
+			if(a[j-1] > tmp) {
+				a[j] = a[j-1];
+			}
+			else {
+				a[j] = tmp;
+				break;
+			}
+		}
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	int n;
@@ -11,9 +28,12 @@ int main(int argc, char const *argv[])
 		cin >> a[i];
 	}
 
+	inseration_sort(a, n);
+
 	for (int i = 0; i < n; ++i)
 	{
-		cout << a[i];
+		cout << a[i] << " ";
 	}
+	cout << endl;
 	return 0;
 }
